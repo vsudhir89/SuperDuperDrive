38e467c5f8a6d4e3edb63fec4bb0381cdcb96d16
Sudhir Vaidya <v.sudhir89@gmail.com>
2020-07-18 18:25:31 -0700

    Configure Security of the SuperSuperDrive application
    Configures security of signup, login and home pages

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/SuperDuperDriveSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.udacity.jwdnd.course1.cloudstorage.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SuperDuperDriveSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+      http.authorizeRequests().antMatchers("/h2/**").permitAll()
+              .and().csrf().ignoringAntMatchers("/h2/**")
+              .and().headers().frameOptions().sameOrigin();
+
+      http.authorizeRequests()
+              .antMatchers("/signup", "/css/**", "/js/**").permitAll()
+              .anyRequest().authenticated();
+
+      http.formLogin()
+              .loginPage("/login")
+              .permitAll();
+
+      http.formLogin()
+              .defaultSuccessUrl("/home", true);
+      // TODO: check if there are any logout specific configs to be done
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	super.configure(auth);
+  }
+}
