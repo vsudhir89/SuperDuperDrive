5ce1107cad8ad6c7816ef071e4eb2a15a483586f
Sudhir Vaidya <v.sudhir89@gmail.com>
2020-07-21 11:11:48 -0700

    Update Authentication service to properly handle logged in user with authorities

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
@@ -6,9 +6,12 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AuthenticationService implements AuthenticationProvider {
@@ -23,15 +26,18 @@ public class AuthenticationService implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String username = authentication.getName();
-        String password = authentication.getCredentials().toString();
-        User user = userMapper.getUser(username);
-        if (user != null) {
-            String encodedSalt = user.getSalt();
-            String hashedPassword = hashService.getHashedValue(password, encodedSalt);
-            if (user.getPassword().equals(hashedPassword)) {
-                // successful authentication
-		return new UsernamePasswordAuthenticationToken(username, password, new ArrayList());
+	String username = authentication.getName();
+	String password = authentication.getCredentials().toString();
+	User user = userMapper.getUser(username);
+	if (user != null) {
+	    String encodedSalt = user.getSalt();
+	    String hashedPassword = hashService.getHashedValue(password, encodedSalt);
+	    if (user.getPassword().equals(hashedPassword)) {
+		// successful authentication
+		List<GrantedAuthority> grantedAuthorityList = new ArrayList<GrantedAuthority>();
+		grantedAuthorityList.add(new SimpleGrantedAuthority("ROLE_USER"));
+		User loggedInUser = new User(user.getUserid(), user.getUsername(), user.getSalt(), user.getPassword(), user.getFirstName(), user.getLastName());
+		return new UsernamePasswordAuthenticationToken(loggedInUser, "", grantedAuthorityList);
 	    }
 	}
 	return null;
